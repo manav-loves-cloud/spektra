76f152afeb1a0658e3d713fd663dabef84ff3e54
bhavangowdan <83076341+bhavangowdan@users.noreply.github.com>
2021-08-24 17:49:42 +0530

    Add files via upload

---
+++ onbording/common/powershellscr/ps1.ps1
@@ -0,0 +1,31 @@
+﻿New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\" -Name "Reliability" –Force
+New-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Reliability' `
+                 -Name ShutdownReasonOn `
+                 -Value 0x00000000 `
+                 -PropertyType DWORD `
+                 -Force
+
+
+New-Item -Path "HKLM:\SOFTWARE\WOW6432Node\Policies\Microsoft\Windows NT\" -Name "Reliability" –Force
+New-ItemProperty -Path 'HKLM:\SOFTWARE\WOW6432Node\Policies\Microsoft\Windows NT\Reliability' `
+                 -Name ShutdownReasonOn `
+                 -Value 0x00000000 `
+                 -PropertyType DWORD `
+                 -Force
+sleep 5
+
+#Download git repository 
+New-Item -ItemType directory -Path C:\AllFiles
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile("https://github.com/MicrosoftLearning/AZ-304-Microsoft-Azure-Architect-Design/archive/master.zip","C:\AllFiles\AllFiles.zip")
+#unziping folder
+function Expand-ZIPFile($file, $destination)
+{
+$shell = new-object -com shell.application
+$zip = $shell.NameSpace($file)
+foreach($item in $zip.items())
+{
+$shell.Namespace($destination).copyhere($item)
+}
+}
+Expand-ZIPFile -File "C:\AllFiles\AllFiles.zip" -Destination "C:\AllFiles\"
---
+++ onbording/common/powershellscr/pss1.ps1
@@ -0,0 +1,65 @@
+﻿Param (
+    [Parameter(Mandatory = $true)]
+    [string]
+    $AzureUserName,
+
+    [string]
+    $AzurePassword,
+
+    [string]
+    $AzureTenantID,
+
+    [string]
+    $AzureSubscriptionID,
+
+    [string]
+    $ODLID,
+
+    [string]
+    $InstallCloudLabsShadow,
+
+    [string]
+    $DeploymentID
+)
+
+Start-Transcript -Path C:\WindowsAzure\Logs\CloudLabsCustomScriptExtension.txt -Append
+[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls" 
+
+#Import Common Functions
+$path = pwd
+$path=$path.Path
+$commonscriptpath = "$path" + "\cloudlabs-common\cloudlabs-windows-functions.ps1"
+. $commonscriptpath
+
+#Create C:\CloudLabs
+New-Item -ItemType directory -Path C:\CloudLabs -Force
+
+# Run Imported functions from cloudlabs-windows-functions.ps1
+WindowsServerCommon
+InstallCloudLabsShadow $ODLID $InstallCloudLabsShadow
+CreateCredFile $AzureUserName $AzurePassword $AzureTenantID $AzureSubscriptionID
+
+sleep 5
+
+InstallAzPowerShellModule
+InstallAzCLI
+InstallEdgeChromium
+
+sleep 10
+
+#Download git repository
+New-Item -ItemType directory -Path C:\AllFiles
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile("https://github.com/MicrosoftLearning/AZ-304-Microsoft-Azure-Architect-Design/archive/master.zip","C:\AllFiles\AllFiles.zip")
+#unziping folder
+function Expand-ZIPFile($file, $destination)
+{
+$shell = new-object -com shell.application
+$zip = $shell.NameSpace($file)
+foreach($item in $zip.items())
+{
+$shell.Namespace($destination).copyhere($item)
+}
+}
+Expand-ZIPFile -File "C:\AllFiles\AllFiles.zip" -Destination "C:\AllFiles\"
