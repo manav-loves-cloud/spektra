61f30d5a9eac9b1ae891ffe33f6a5c539994f123
bhavangowdan <83076341+bhavangowdan@users.noreply.github.com>
2021-08-25 14:11:01 +0530

    Delete extract zip.ps1

--- onbording/common/powershellscr/extract zip.ps1
+++
@@ -1,4 +0,0 @@
-﻿New-Item -ItemType directory -Path C:\AllFiles
-$WebClient = New-Object System.Net.WebClient
-$WebClient.DownloadFile("https://github.com/himanshuahlawat31/udacity-esnd/raw/main/Esnd-4.zip","C:\AllFiles\AllFiles.zip")
-Expand-Archive -LiteralPath C:\AllFiles\AllFiles.zip -DestinationPath C:\AllFiles
