3d9534024f6fb151f696e7b443e88e5d5fd4759d
bhavangowdan <83076341+bhavangowdan@users.noreply.github.com>
2022-04-08 19:11:18 +0530

    Create main.tf

---
+++ clouddevops/clouddevopsc1/main.tf
@@ -0,0 +1,54 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/22"]
+  location            = "East US"
+  resource_group_name = "Azuredevops"
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = "Azuredevops"
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
+  resource_group_name = "Azuredevops"
+  location            = "East US"
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "main" {
+  name                            = "${var.prefix}-vm"
+  resource_group_name             = "Azuredevops"
+  location                        = "East US"
+  size                            = "Standard_B1s"
+  admin_username                  = "bhavan"
+  admin_password                  = "Bhavan@12345"
+  disable_password_authentication = false
+  network_interface_ids = [
+    azurerm_network_interface.main.id,
+  ]
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+}
