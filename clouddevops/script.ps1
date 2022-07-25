c58ed5c5778e62d7165bd94c7c9a928f06be9ffc
bhavangowdan <83076341+bhavangowdan@users.noreply.github.com>
2022-07-25 09:57:41 +0530

    Update script.ps1

--- clouddevops/script.ps1
+++ clouddevops/script.ps1
@@ -1,8 +1,39 @@
+
 Start-Transcript -Path C:\WindowsAzure\Logs\CloudLabsCustomScriptExtension.txt -Append
 
-InstallChocolatey
+Param (
+    [Parameter(Mandatory = $true)]
+    [string]
+    $AzureUserName,
 
-InstallAzPowerShellModule
+    [string]
+    $AzurePassword,
+
+    [string]
+    $AzureTenantID,
+
+    [string]
+    $AzureSubscriptionID,
+
+    [string]
+    $DeploymentID
+  )
+
+Start-Transcript -Path C:\WindowsAzure\Logs\CloudLabsCustomScriptExtension.txt -Append
+[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls" 
+
+#import common functions
+$path = pwd
+$path=$path.Path
+$commonscriptpath = "$path" + "\cloudlabs-common\cloudlabs-windows-functions.ps1"
+. $commonscriptpath
+
+
+InstallChocolatey
+InstallAzCLI
+CreateCredFile $AzureUserName $AzurePassword $AzureTenantID $AzureSubscriptionID $DeploymentID
+
 
 
 Clear-Host 
