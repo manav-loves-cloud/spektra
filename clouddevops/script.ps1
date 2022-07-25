c7db141f84bf5c1bb912055779f0448a779ae661
bhavangowdan <83076341+bhavangowdan@users.noreply.github.com>
2022-07-25 19:25:18 +0530

    Update script.ps1

--- clouddevops/script.ps1
+++ clouddevops/script.ps1
@@ -48,15 +48,7 @@ $WebClient.DownloadFile("https://raw.githubusercontent.com/bhavangowdan/spektra/
  $WebClient = New-Object System.Net.WebClient
 $WebClient.DownloadFile("https://raw.githubusercontent.com/bhavangowdan/spektra/main/clouddevops/passwordupdate.ps1","C:\LabFiles\passwordupdate.ps1")
 
-. C:\LabFiles\passupdate.ps1
-$updatedpassword=$uppassword
-
-. C:\LabFiles\AzureCreds.ps1
-
-az Login -u  $AzureUserName -p $AzurePassword
-
-az vm user update -u cyberadmin -p $updatedpassword -n CYBERND0301 -g cyber-$DeploymentID
-
+Powershell.exe -File C:\LabFiles\passwordupdate.ps1
 
 Clear-Host 
 $ErrorActionPreference = "SilentlyContinue" 
