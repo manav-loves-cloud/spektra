96611c49c3ed28ee985ad661e541aee1cda130d1
bhavangowdan <83076341+bhavangowdan@users.noreply.github.com>
2023-04-04 15:19:25 +0530

    Update psscript2.ps1

--- loanmodelling/psscript2.ps1
+++ loanmodelling/psscript2.ps1
@@ -221,4 +221,24 @@ $HeadersInfo = Invoke-AzSynapsePipeline -WorkspaceName $WorkspaceName -PipelineN
 $HeadersInfo
 $HeadersRunID=$HeadersInfo.RunId
 
-sleep 10
+sleep 600
+
+#Running the spark note book
+
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile("https://raw.githubusercontent.com/bhavangowdan/spektra/main/loanmodelling/Notebook%201.ipynb","C:\LabFiles\Notebook1.ipynb")
+$WebClient.DownloadFile("https://raw.githubusercontent.com/bhavangowdan/spektra/main/loanmodelling/Pipeline%201.json","C:\LabFiles\Pipeline1.json")
+
+(Get-Content -Path "C:\LabFiles\Notebook1.ipynb") | ForEach-Object {$_ -Replace 'DID', $DeploymentID} | Set-Content -Path "C:\LabFiles\Notebook1.ipynb"
+
+
+sleep 100
+
+Set-AzSynapseNotebook -WorkspaceName $workspacename -Name notebook1 -DefinitionFile "C:\LabFiles\Notebook1.ipynb"
+
+Set-AzSynapsePipeline -WorkspaceName $workspacename -Name pipeline1 -DefinitionFile "C:\LabFiles\Pipeline1.json"
+
+
+Invoke-AzSynapsePipeline -WorkspaceName $workspacename -PipelineName pipeline1
+
+sleep 600
